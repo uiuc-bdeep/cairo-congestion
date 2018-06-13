9de5cd63576202a875df9a0ce1491aaada7236a0
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-13 13:37:21 -0500

    update 06/13/2018

--- visualize.R
+++ visualize.R
@@ -33,7 +33,7 @@ lapply(packages, pkgTest)
 
 # data from latest test run of crawler
 
-test.path <- "cairo-congestion-20180607.csv"
+test.path <- "cairo-congestion-20180613.csv"
 
 # read data 
 
@@ -47,6 +47,7 @@ Cairo_map <- get_map(location = Cairo, maptype = "roadmap", source = "google", z
 # plot map of origin points
 
 ggmap(Cairo_map) + 
+  ggtitle("Test Run 06/08/2018 - 06/13/2018") +
   geom_point(aes(y = origin_lat, x = origin_long), size = 0.7, color = "blue",data = test) +
   theme(axis.ticks = element_blank(),
         axis.text = element_blank(),
@@ -62,7 +63,7 @@ test$driving_duration_in_traffic <- test$driving_duration_in_traffic / 60
 
 ggplot(test) +
   geom_histogram(aes(driving_duration_in_traffic), binwidth = 1) +
-  ggtitle("Cairo Crawler Test", subtitle = "Test Run 06/07/2018") + 
+  ggtitle("Cairo Crawler Test", subtitle = "Test Run 06/08/2018 - 06/13/2018") + 
   xlab("Duration (Minutes)") +
   ylab("Number of Trips") +
   theme_bw() 
@@ -93,21 +94,12 @@ test1$cairo_time <- as.POSIXct(test1$cairo_time, format = "%H:%M:%S")
 
 ggplot() + 
     geom_line(mapping = aes(x = cairo_time, y = mean), data = test1) +
+    geom_point(mapping = aes(x = cairo_time, y = mean), data = test1) +
     xlab("Time of Day") + ylab("Average Duration /n (Minutes)") +
-    ggtitle("Average Duration Over Time", "Test Run 06/07/2018") + 
+    ggtitle("Average Duration Over Time", "Test Run 06/08/2018 - 06/13/2018") + 
     theme_bw()
   
 ggsave("time-of-day.png", height = 5, width = 8, dpi = 300)
-
-# group trips by grid cell
-
-test <- group_by(test, cairo_time, id)
-
-test1 <- summarise(test, 
-                   mean = mean(driving_duration_in_traffic), 
-                   sd = sd(driving_duration_in_traffic))
-
-ggsave("time-of-day-grid.png", height = 5, width = 8, dpi = 300)
 
 # generate trip IDs
 
@@ -132,8 +124,8 @@ m1 <- felm(driving_duration_in_traffic ~ cairo_time | trip_id, data = test)
 
 coef <- as.data.frame(summary(m1)$coefficients)
 
-coef <- cbind(test1$cairo_time[2:31], coef)
-names(coef)[names(coef) == "test1$cairo_time[2:31]"] <- "cairo_time"
+coef <- cbind(test1$cairo_time[2:102], coef)
+names(coef)[names(coef) == "test1$cairo_time[2:102]"] <- "cairo_time"
 
 coef$cairo_time <- format(coef$cairo_time, format = "%H:%M:%S")
 coef$cairo_time <- as.POSIXct(coef$cairo_time, format = "%H:%M:%S")
@@ -145,7 +137,7 @@ coef$ci2 <- coef$Estimate + 1.96 * coef$`Std. Error`
 
 ggplot() + 
   ggtitle("Regress duration on time of day",
-          subtitle = "with Trip FEs, Test Run 06/07/2018") +
+          subtitle = "with Trip FEs, Test Run 06/08/2018 - 06/13/2018") +
   ylab("Estimate") +
   xlab("Time of Day") +
   geom_line(mapping = aes(x = cairo_time, y = Estimate), data = coef) + 
